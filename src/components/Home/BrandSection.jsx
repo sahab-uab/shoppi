1449d99a5d1cd6bdf27748786fe1c167544758f8
Munna <mayinuddin.munna@gmail.com>
2025-06-23 16:43:42 +0600

    brand slider added

--- src/components/Home/BrandSection.jsx
+++ src/components/Home/BrandSection.jsx
@@ -1,44 +1,72 @@
 import Image from "next/image";
 import Link from "next/link";
 import DataIteration from "../Helpers/DataIteration";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
 export default function BrandSection({ className, sectionTitle, brands = [] }) {
   return (
     <div data-aos="fade-up" className={`w-full ${className || ""}`}>
       <div className="container-x mx-auto">
-        <div className=" section-title flex justify-between items-center mb-5">
-          <div>
-            <h1 className="sm:text-3xl text-xl font-600 text-qblacktext">
-              {sectionTitle}
-            </h1>
-          </div>
+        <div className="section-title flex justify-between items-center mb-5">
+          <h1 className="sm:text-3xl text-xl font-semibold text-qblacktext">
+            {sectionTitle}
+          </h1>
         </div>
-        <div className="grid lg:grid-cols-6 sm:grid-cols-4 grid-cols-2">
-          <DataIteration
-            datas={brands}
-            startLength={0}
-            endLength={brands.length}
-          >
-            {({ datas }) => (
-              <div key={datas.id} className="item">
-                <Link
-                  href={{
-                    pathname: "/products",
-                    query: { brand: datas.slug },
-                  }}
-                >
-                  <div className="w-full h-[130px] bg-white border border-primarygray flex justify-center items-center relative cursor-pointer">
-                    <Image
-                      layout="fill"
-                      objectFit="scale-down"
-                      src={`${process.env.NEXT_PUBLIC_BASE_URL + datas.logo}`}
-                      alt={datas.name}
-                    />
-                  </div>
-                </Link>
-              </div>
-            )}
-          </DataIteration>
-        </div>
+
+        <Swiper
+          spaceBetween={20}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{
+            320: {
+              slidesPerView: 2,
+              spaceBetween: 10,
+            },
+            640: {
+              slidesPerView: 3,
+              spaceBetween: 15,
+            },
+            768: {
+              slidesPerView: 4,
+              spaceBetween: 20,
+            },
+            1024: {
+              slidesPerView: 5,
+              spaceBetween: 30,
+            },
+            1280: {
+              slidesPerView: 6,
+              spaceBetween: 30,
+            },
+          }}
+          modules={[Autoplay]}
+          className="brand-swiper"
+        >
+          {brands?.map((brand) => (
+            <SwiperSlide key={brand.id}>
+              <Link
+                href={{
+                  pathname: "/products",
+                  query: { brand: brand.slug },
+                }}
+              >
+                <div className="w-full h-[130px] bg-white border border-gray-200 flex justify-center items-center relative cursor-pointer rounded hover:shadow-md transition">
+                  <Image
+                    src={process.env.NEXT_PUBLIC_BASE_URL + brand.logo}
+                    alt={brand.name}
+                    fill
+                    style={{ objectFit: "scale-down" }}
+                  />
+                </div>
+              </Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
