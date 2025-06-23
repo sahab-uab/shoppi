433706ccebe50f303ca85954f4e7d881cda1124d
Munna <mayinuddin.munna@gmail.com>
2025-06-23 08:09:19 +0600

    footer done

--- src/components/Partials/Footers/Footer/index.jsx
+++ src/components/Partials/Footers/Footer/index.jsx
@@ -6,6 +6,7 @@ import Facebook from "../../../Helpers/icons/Facebook";
 import Instagram from "../../../Helpers/icons/Instagram";
 import Youtube from "../../../Helpers/icons/Youtube";
 import FontAwesomeCom from "../../../Helpers/icons/FontAwesomeCom";
+
 export default function Footer({ settings }) {
   const { websiteSetup } = useSelector((state) => state.websiteSetup);
   const [firstCol, setFirstCol] = useState(null);
@@ -61,67 +62,43 @@ export default function Footer({ settings }) {
   });
 
   return (
-    <footer className="footer-section-wrapper bg-white print:hidden">
-      <div className="container-x block mx-auto pt-[56px]">
-        <div className="w-full flex flex-col items-center mb-[50px]">
-          {/* logo area */}
-          <div className="mb-[40px]">
-            <Link href="/" passHref legacyBehavior>
-              <a>
-                {settings && (
-                  <Image
-                    width="153"
-                    height="44"
-                    objectFit="scale-down"
-                    src={`${process.env.NEXT_PUBLIC_BASE_URL + settings.logo}`}
-                    alt="logo"
-                  />
-                )}
-              </a>
-            </Link>
-          </div>
-          <div className="w-full h-[1px] bg-[#E9E9E9]"></div>
-        </div>
-        <div className="lg:flex justify-between mb-[50px]">
-          <div className="lg:w-[424px]  ml-0 w-full mb-10 lg:mb-0">
-            <h1 className="text-[18] font-500 text-[#2F2F2F] mb-5">About Us</h1>
-            <p className="text-[#9A9A9A] text-[15px] w-[247px] leading-[28px]">
-              {footerContent && footerContent.about_us}
-            </p>
-            {/*<p className="text-[#9A9A9A] text-[15px] w-[247px] leading-[28px]">*/}
-            {/*  {footerContent && footerContent.email ? footerContent.email : ""}*/}
-            {/*</p>*/}
-            {/*<p className="text-[#9A9A9A] text-[15px] w-[247px] leading-[28px]">*/}
-            {/*  {footerContent && footerContent.address*/}
-            {/*    ? footerContent.address*/}
-            {/*    : ""}*/}
-            {/*</p>*/}
-            {/*<div>*/}
-            {/*  <ul className="flex flex-col space-y-4 ">*/}
-            {/*    <li>*/}
-            {/*      <Link href="/tracking-order">*/}
-            {/*        <span className="text-[#9A9A9A] text-[15px] hover:text-qblack border-b border-transparent hover:border-qblack cursor-pointer capitalize">*/}
-            {/*          Track Order*/}
-            {/*        </span>*/}
-            {/*      </Link>*/}
-            {/*    </li>*/}
-            {/*    <li>*/}
-            {/*      <Link href="/faq">*/}
-            {/*        <span className="text-[#9A9A9A] text-[15px] hover:text-qblack border-b border-transparent hover:border-qblack cursor-pointer capitalize">*/}
-            {/*          Support*/}
-            {/*        </span>*/}
-            {/*      </Link>*/}
-            {/*    </li>*/}
-            {/*    */}
-            {/*  </ul>*/}
-            {/*</div>*/}
-          </div>
-          <div className="flex-1 lg:flex">
-            <div className="lg:w-1/3 w-full mb-10 lg:mb-0">
+    <>
+      <footer className="bg-black text-white">
+        <div className="container-x block mx-auto pt-[56px]">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+            {/* Exclusive Section */}
+            <div className="lg:col-span-1">
+              {/* logo area */}
+              <div className="mb-[40px]">
+                <Link href="/" passHref legacyBehavior>
+                  <a>
+                    {settings && (
+                      <Image
+                        width="153"
+                        height="44"
+                        objectFit="scale-down"
+                        src={`${
+                          process.env.NEXT_PUBLIC_BASE_URL + settings.logo
+                        }`}
+                        alt="logo"
+                      />
+                    )}
+                  </a>
+                </Link>
+              </div>
+              <h4 className="text-lg mb-4 font-bold">About Us</h4>
+              <p className="text-gray-300 mb-4">
+                We know there are a lot of three developers our but we pride
+                into a firm in the industry.
+              </p>
+            </div>
+
+            {/* Account Section */}
+            <div className="lg:col-span-1">
               {firstCol && (
                 <>
                   <div className="mb-5">
-                    <h6 className="text-[18] font-500 text-[#2F2F2F]">
+                    <h6 className="text-[18] font-500 text-white">
                       {firstCol.columnTitle}
                     </h6>
                   </div>
@@ -132,7 +109,7 @@ export default function Footer({ settings }) {
                           <li key={i}>
                             <Link href={item.link} passHref legacyBehavior>
                               <a rel="noopener noreferrer">
-                                <span className="text-[#9A9A9A] text-[15px] hover:text-qblack border-b border-transparent hover:border-qblack cursor-pointer capitalize">
+                                <span className="text-[#cacaca] text-[15px] hover:text-qblack border-b border-transparent hover:border-qblack cursor-pointer capitalize">
                                   {item.title}
                                 </span>
                               </a>
@@ -144,119 +121,118 @@ export default function Footer({ settings }) {
                 </>
               )}
             </div>
-            <div className="lg:w-1/3 lg:flex lg:flex-col items-center w-full mb-10 lg:mb-0 ">
-              <div>
-                {secondCol && (
-                  <>
-                    <div className="mb-5">
-                      <h6 className="text-[18] font-500 text-[#2F2F2F]">
-                        {secondCol.columnTitle}
-                      </h6>
-                    </div>
-                    <div>
-                      <ul className="flex flex-col space-y-4 ">
-                        {secondCol.col_links.length > 0 &&
-                          secondCol.col_links.map((item, i) => (
-                            <li key={i}>
-                              <Link href={item.link} passHref legacyBehavior>
-                                <a rel="noopener noreferrer">
-                                  <span className="text-[#9A9A9A] text-[15px] hover:text-qblack border-b border-transparent hover:border-qblack cursor-pointer capitalize">
-                                    {item.title}
-                                  </span>
-                                </a>
-                              </Link>
-                            </li>
-                          ))}
-                      </ul>
-                    </div>
-                  </>
-                )}
-              </div>
+
+            {/* General Link Section */}
+            <div className="lg:col-span-1">
+              {secondCol && (
+                <>
+                  <div className="mb-5">
+                    <h6 className="text-[18] font-500 text-white">
+                      {secondCol.columnTitle}
+                    </h6>
+                  </div>
+                  <div>
+                    <ul className="flex flex-col space-y-4 ">
+                      {secondCol.col_links.length > 0 &&
+                        secondCol.col_links.map((item, i) => (
+                          <li key={i}>
+                            <Link href={item.link} passHref legacyBehavior>
+                              <a rel="noopener noreferrer">
+                                <span className="text-[#cacaca] text-[15px] hover:text-qblack border-b border-transparent hover:border-qblack cursor-pointer capitalize">
+                                  {item.title}
+                                </span>
+                              </a>
+                            </Link>
+                          </li>
+                        ))}
+                    </ul>
+                  </div>
+                </>
+              )}
             </div>
-            <div className="lg:w-1/3 lg:flex lg:flex-col items-center w-full mb-10 lg:mb-0">
-              <div>
-                {thirdCol && (
-                  <>
-                    <div className="mb-5">
-                      <h6 className="text-[18] font-500 text-[#2F2F2F]">
-                        {thirdCol.columnTitle}
-                      </h6>
-                    </div>
-                    <div>
-                      <ul className="flex flex-col space-y-4 ">
-                        {thirdCol.col_links.length > 0 &&
-                          thirdCol.col_links.map((item, i) => (
-                            <li key={i}>
-                              <Link href={item.link} passHref legacyBehavior>
-                                <a rel="noopener noreferrer">
-                                  <span className="text-[#9A9A9A] text-[15px] hover:text-qblack border-b border-transparent hover:border-qblack cursor-pointer capitalize">
-                                    {item.title}
-                                  </span>
-                                </a>
-                              </Link>
-                            </li>
-                          ))}
-                      </ul>
-                    </div>
-                  </>
-                )}
-              </div>
+
+            {/* Helpful Section */}
+            <div className="lg:col-span-1">
+              {thirdCol && (
+                <>
+                  <div className="mb-5">
+                    <h6 className="text-[18] font-500 text-white">
+                      {thirdCol.columnTitle}
+                    </h6>
+                  </div>
+                  <div>
+                    <ul className="flex flex-col space-y-4 ">
+                      {thirdCol.col_links.length > 0 &&
+                        thirdCol.col_links.map((item, i) => (
+                          <li key={i}>
+                            <Link href={item.link} passHref legacyBehavior>
+                              <a rel="noopener noreferrer">
+                                <span className="text-[#cacaca] text-[15px] hover:text-qblack border-b border-transparent hover:border-qblack cursor-pointer capitalize">
+                                  {item.title}
+                                </span>
+                              </a>
+                            </Link>
+                          </li>
+                        ))}
+                    </ul>
+                  </div>
+                </>
+              )}
             </div>
+
+            {/* Bottom part */}
           </div>
-        </div>
-        <div
-          className="bottom-bar border-t border-qgray-border lg:h-[82px] flex lg:flex-row flex-col-reverse
+
+          <div
+            className="bottom-bar lg:h-[82px] flex lg:flex-row flex-col-reverse
          justify-between items-center"
-        >
-          <div className="flex rtl:space-x-reverse lg:space-x-5 space-x-2.5 justify-between items-center mb-3">
-            <div className="flex rtl:space-x-reverse space-x-5 items-center">
-              {socialLink &&
-                socialLink.length > 0 &&
-                socialLink.map((item, i) => (
-                  <a key={i} href={item.link} target="_blank" rel="noreferrer">
-                    <FontAwesomeCom
-                      className="w-4 h-4 text-qgray"
-                      icon={item.icon}
+          >
+            <div className="flex rtl:space-x-reverse lg:space-x-5 space-x-2.5 justify-between items-center mb-3">
+              <div className="flex rtl:space-x-reverse space-x-5 items-center">
+                {socialLink &&
+                  socialLink.length > 0 &&
+                  socialLink.map((item, i) => (
+                    <a
+                      key={i}
+                      href={item.link}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <FontAwesomeCom
+                        className="w-4 h-4 text-qgray"
+                        icon={item.icon}
+                      />
+                    </a>
+                  ))}
+              </div>
+              <span className="sm:text-base text-[10px] text-[#cacaca]">
+                {footerContent && footerContent.copyright
+                  ? footerContent.copyright
+                  : ""}
+              </span>
+            </div>
+            {footerContent && footerContent.payment_image ? (
+              <div className="mt-2 lg:mt-0">
+                <Link href="#" passHref legacyBehavior>
+                  <a>
+                    <Image
+                      width="318"
+                      height="28"
+                      src={`${
+                        process.env.NEXT_PUBLIC_BASE_URL +
+                        footerContent.payment_image
+                      }`}
+                      alt="payment-getways"
                     />
                   </a>
-                  // <Link key={i} href={item.link} passHref legacyBehavior>
-                  //   <a rel="noopener noreferrer" key={i}>
-                  //     <FontAwesomeCom
-                  //       className="w-4 h-4 text-qgray"
-                  //       icon={item.icon}
-                  //     />
-                  //   </a>
-                  //   Anchor
-                  // </Link>
-                ))}
-            </div>
-            <span className="sm:text-base text-[10px] text-qgray font-300">
-              {footerContent && footerContent.copyright
-                ? footerContent.copyright
-                : ""}
-            </span>
+                </Link>
+              </div>
+            ) : (
+              ""
+            )}
           </div>
-          {footerContent && footerContent.payment_image ? (
-            <div className="mt-2 lg:mt-0">
-              <Link href="#" passHref legacyBehavior>
-                <a>
-                  <Image
-                    width="318"
-                    height="28"
-                    src={`${
-                      process.env.NEXT_PUBLIC_BASE_URL +
-                      footerContent.payment_image
-                    }`}
-                    alt="payment-getways"
-                  />
-                </a>
-              </Link>
-            </div>
-          ) : (
-            ""
-          )}
         </div>
-      </div>
-    </footer>
+      </footer>
+    </>
   );
 }
